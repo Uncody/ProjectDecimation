6fdd16d9e29a8166cb69ea4d256d91d6f8e66166
Uncody <timatop2020jjj@gmail.com>
2024-09-01 11:44:50 +0300

    сосать

---
+++ src/client/java/com/decimation/ProjectDecimationClient.java
@@ -0,0 +1,13 @@
+package com.decimation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectDecimationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		ProjectDecimation.LOGGER.info("five niggers in uran ass");
+	}
+}
+
+
+
