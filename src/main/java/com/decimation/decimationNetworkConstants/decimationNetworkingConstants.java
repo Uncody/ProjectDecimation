84df75342a8a3f336d719fe953cbfd9984ecbca0
Uncody <timatop2020jjj@gmail.com>
2024-09-04 21:40:29 +0300

    some things that I will delete later

---
+++ src/main/java/com/decimation/decimationNetworkConstants/decimationNetworkingConstants.java
@@ -0,0 +1,8 @@
+package com.decimation.decimationNetworkConstants;
+import net.minecraft.util.Identifier;
+
+
+
+public class decimationNetworkingConstants {
+    public static final Identifier TEST_PACKET_ID = Identifier.of("decimation", "lockpick");
+}
