6d290569eb26a978c96bdf1db04adcb819b313c0
TheUran <xb7995699@gmail.com>
2024-09-16 13:36:57 +0300

    Initial coomit

--- src/main/java/com/decimation/DecimationMod.java
+++ src/main/java/com/decimation/DecimationMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.decimation;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,25 +7,20 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+@Mod(modid = DecimationMod.MODID, name = DecimationMod.NAME, version = DecimationMod.VERSION)
+public class DecimationMod {
+    public static final String MODID = "Decimation";
+    public static final String NAME = "Project Decimation";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    public void init(FMLInitializationEvent event) {
     }
 }
