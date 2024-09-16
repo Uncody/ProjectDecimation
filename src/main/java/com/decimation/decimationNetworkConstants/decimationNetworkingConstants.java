6a3d11f1ee1c9baea4977de0a4c49be5e2c4bd26
TheUranofficial <101169411+TheUranofficial@users.noreply.github.com>
2024-09-16 13:07:35 +0300

    Delete src directory

--- src/main/java/com/decimation/decimationNetworkConstants/decimationNetworkingConstants.java
+++
@@ -1,8 +0,0 @@
-package com.decimation.decimationNetworkConstants;
-import net.minecraft.util.Identifier;
-
-
-
-public class decimationNetworkingConstants {
-    public static final Identifier TEST_PACKET_ID = Identifier.of("decimation", "lockpick");
-}
